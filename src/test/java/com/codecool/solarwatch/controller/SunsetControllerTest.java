35b9c7b4c6bda67d62197ca74d7ddc480705107b
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-05 09:12:03 +0100

    Create integration tests for SunsetController for saving sunsets

--- src/test/java/com/codecool/solarwatch/controller/SunsetControllerTest.java
+++ src/test/java/com/codecool/solarwatch/controller/SunsetControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +20,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,6 +59,46 @@ class SunsetControllerTest {
         this.mockMvc.perform(get(String.format("/api/sunset?city=%s&date=%s", expectedCityName, expectedDate)))
                 .andExpect(status().isOk())
                 .andExpect(content().string(expected));
+    }
+
+
+    @Test
+    @WithMockUser(username = "user1", roles = {"USER"})
+    void createSunrise_UserWithUSERRole_ShouldReturn401UnAuthorizeError() throws Exception {
+        String createdSunrise = "{\"date\": \"2024-02-24\",\"sunset\": \"4 PM\",\"cityId\": 1}";
+
+        this.mockMvc.perform(post("/api/sunset")
+                .contentType(MediaType.APPLICATION_JSON).content(createdSunrise)).andExpect(status().isForbidden());
+    }
+
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void createSunrise_UserWithADMINRole_ShouldReturn201Created() throws Exception {
+        String createdSunrise = "{\"date\": \"2024-02-24\",\"sunset\": \"4 PM\",\"cityId\": 1}";
+        City expectedCity = new City(
+                "Los Angeles",
+                "California",
+                "USA",
+                -118.243683,
+                34.052235,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+        when(cityRepository.findById(1L)).thenReturn(Optional.of(expectedCity));
+
+        this.mockMvc.perform(post("/api/sunset")
+                .contentType(MediaType.APPLICATION_JSON).content(createdSunrise)).andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void createSunrise_UserWithADMINRoleCreatesASunriseWithNonExistingCityId_ShouldReturn409AndNonExistingCityErrorMessage() throws Exception {
+        String createdSunrise = "{\"date\": \"2024-02-24\",\"sunset\": \"4 PM\",\"cityId\": 1}";
+        when(cityRepository.findById(1L)).thenReturn(Optional.empty());
+        this.mockMvc.perform(post("/api/sunset")
+                        .contentType(MediaType.APPLICATION_JSON).content(createdSunrise)).andExpect(status().isConflict())
+                .andExpect(content().string("City with id: 1 doesn't exists!"));
     }
 
 }
