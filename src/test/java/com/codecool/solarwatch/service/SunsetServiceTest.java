6db8163aab193b6186270dc336447bba2b3bca65
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-06 09:48:57 +0100

    Create unit test for SunsetService

--- src/test/java/com/codecool/solarwatch/service/SunsetServiceTest.java
+++ src/test/java/com/codecool/solarwatch/service/SunsetServiceTest.java
@@ -1,15 +1,19 @@
 package com.codecool.solarwatch.service;
 
+import com.codecool.solarwatch.custom_exception.NonExistingCityException;
+import com.codecool.solarwatch.custom_exception.NonExistingSunsetException;
 import com.codecool.solarwatch.model.entities.city.City;
 import com.codecool.solarwatch.model.entities.sunset.SunsetEntity;
-import com.codecool.solarwatch.model.sunset.Sunset;
-import com.codecool.solarwatch.model.sunset.SunsetReport;
-import com.codecool.solarwatch.model.sunset.SunsetResult;
+import com.codecool.solarwatch.model.sunrise.Sunrise;
+import com.codecool.solarwatch.model.sunset.*;
+import com.codecool.solarwatch.repository.CityRepository;
 import com.codecool.solarwatch.repository.SunsetRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,71 +22,162 @@ import static org.mockito.Mockito.when;
 
 class SunsetServiceTest {
 
-    /*
+    private RestTemplate restTemplateMock;
+    private SunsetRepository sunsetRepository;
+    private CityRepository cityRepositoryMock;
+
+    @BeforeEach
+    void CreateMockDependencies(){
+        restTemplateMock = mock(RestTemplate.class);
+        sunsetRepository = mock(SunsetRepository.class);
+        cityRepositoryMock = mock(CityRepository.class);
+    }
+
     @Test
-    void getSunset_WithSunriseRepositoryMockedOutAndReturnsAValidSunriseEntity_ShouldReturnSunriseReportForDallasCity(){
+    void getSunrise_SunriseRepositoryShouldReturnEmptyOptional_RestTemplateShouldReturnExpectedSunrise_ShouldReturnSunriseReportWithLosAngeles6AMCurrentDate() {
+        //arrange
         LocalDate expectedDate = LocalDate.now();
+        City expectedCity = new City(
+                "Los Angeles",
+                "California",
+                "USA",
+                1,
+                1,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+        String expectedSunset = "6 AM";
 
-        City expectedCity = new City();
-        expectedCity.setLatitude(1);
-        expectedCity.setLongitude(1);
-        expectedCity.setCountry("USA");
-        expectedCity.setState("Texas");
-        expectedCity.setName("Dallas");
+        String url = String.format("https://api.sunrise-sunset.org/json?lat=%s&lng=%s&date=%s", expectedCity.getLatitude(), expectedCity.getLongitude(), expectedDate);
 
-        SunsetEntity expectedSunsetEntity = new SunsetEntity();
-        expectedSunsetEntity.setDate(expectedDate);
-        expectedSunsetEntity.setSunset("6:30 PM");
-        expectedSunsetEntity.setCity(expectedCity);
+        when(sunsetRepository.findByCityAndDate(expectedCity, expectedDate)).thenReturn(Optional.empty());
+        when(restTemplateMock.getForObject(url, Sunset.class)).thenReturn(
+                new Sunset(
+                        new SunsetResult(expectedSunset)
+                )
+        );
 
-        SunsetRepository sunsetRepositoryMock = mock(SunsetRepository.class);
-        when(sunsetRepositoryMock.findByCityAndDate(expectedCity, expectedDate)).thenReturn(Optional.of(expectedSunsetEntity));
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        SunsetService sunsetService = new SunsetService(sunsetRepositoryMock, restTemplate, cityRepository);
-
+        //act
         SunsetReport result = sunsetService.getSunset(expectedCity, expectedDate);
-        SunsetReport expected = new SunsetReport(expectedCity.getName(), expectedSunsetEntity.getSunset(), expectedDate);
+        SunsetReport expected = new SunsetReport(expectedCity.getName(), expectedSunset, expectedDate);
 
+        //assert
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    void getSunrise_SunriseRepositoryShouldReturnSunsetEntity_ShouldReturnSunriseReportWithLosAngeles6AMCurrentDate() {
+        //arrange
+        LocalDate expectedDate = LocalDate.now();
+        City expectedCity = new City(
+                "Los Angeles",
+                "California",
+                "USA",
+                1,
+                1,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+        String expectedSunset = "6 AM";
+        SunsetEntity expectedSunsetEntity = new SunsetEntity(
+                expectedSunset,
+                expectedDate,
+                expectedCity
+        );
+
+        when(sunsetRepository.findByCityAndDate(expectedCity, expectedDate)).thenReturn(Optional.of(expectedSunsetEntity));
+
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
+
+        //act
+        SunsetReport result = sunsetService.getSunset(expectedCity, expectedDate);
+        SunsetReport expected = new SunsetReport(expectedCity.getName(), expectedSunset, expectedDate);
+
+        //assert
         assertEquals(expected, result);
     }
 
     @Test
-    void getSunset_WithSunriseRepositoryMockedOutAndReturnsAnEmptyOptional_RestTemplateIsMockedOutAndReturnsTheSunriseOfDallas_ShouldReturnSunriseReportForDallasCity(){
+    void getSunrise_SunriseRepositoryShouldReturnEmptyOptional_RestTemplateShouldReturnNull_ShouldThrowNonExistingSunriseException() {
+        //arrange
         LocalDate expectedDate = LocalDate.now();
+        City expectedCity = new City(
+                "Los Angeles",
+                "California",
+                "USA",
+                1,
+                1,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
 
-        City expectedCity = new City();
-        expectedCity.setLatitude(1);
-        expectedCity.setLongitude(1);
-        expectedCity.setCountry("USA");
-        expectedCity.setState("Texas");
-        expectedCity.setName("Dallas");
+        String url = String.format("https://api.sunrise-sunset.org/json?lat=%s&lng=%s&date=%s", expectedCity.getLatitude(), expectedCity.getLongitude(), expectedDate);
 
-        SunsetEntity expectedSunsetEntity = new SunsetEntity();
-        expectedSunsetEntity.setDate(expectedDate);
-        expectedSunsetEntity.setSunset("6:30 PM");
-        expectedSunsetEntity.setCity(expectedCity);
+        when(sunsetRepository.findByCityAndDate(expectedCity, expectedDate)).thenReturn(Optional.empty());
+        when(restTemplateMock.getForObject(url, Sunrise.class)).thenReturn(null);
 
-        SunsetRepository sunsetRepositoryMock = mock(SunsetRepository.class);
-        when(sunsetRepositoryMock.findByCityAndDate(expectedCity, expectedDate)).thenReturn(Optional.empty());
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
 
-        String url = String.format("https://api.sunrise-sunset.org/json?lat=%s&lng=%s&date=%s",expectedCity.getLatitude(),expectedCity.getLongitude(),expectedDate);
-
-        SunsetResult sunsetResult = new SunsetResult(expectedSunsetEntity.getSunset());
-        Sunset expectedSunset = new Sunset(sunsetResult);
-
-        RestTemplate restTemplateMock = mock(RestTemplate.class);
-        when(restTemplateMock.getForObject(url, Sunset.class)).thenReturn(expectedSunset);
-
-
-        SunsetService sunsetService = new SunsetService(sunsetRepositoryMock, restTemplateMock, cityRepository);
-
-        SunsetReport result = sunsetService.getSunset(expectedCity, expectedDate);
-        SunsetReport expected = new SunsetReport(expectedCity.getName(), expectedSunsetEntity.getSunset(), expectedDate);
-
-        assertEquals(expected, result);
+        assertThrows(NonExistingSunsetException.class, () ->  sunsetService.getSunset(expectedCity, expectedDate));
     }
 
-     */
+
+    @Test
+    void createSunrise_CityRepositoryShouldReturnEmptyOptional_ShouldThrowNonExistingCityException() {
+        //arrange
+        LocalDate expectedDate = LocalDate.now();
+        String expectedSunset = "6 AM";
+        SunsetCreationDTO sunsetCreationDTO = new SunsetCreationDTO(expectedDate, expectedSunset, 1);
+
+        when(cityRepositoryMock.findById(1L)).thenReturn(Optional.empty());
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
+
+        assertThrows(NonExistingCityException.class, () -> sunsetService.createSunset(sunsetCreationDTO));
+    }
+
+    @Test
+    void updateSunrise_SunriseRepositoryShouldReturnOptionalOfSunriseEntity_CityRepositoryShouldReturnEmptyOptional_ShouldThrowError() {
+        LocalDate expectedDate = LocalDate.now();
+        String expectedSunset = "6 AM";
+
+        SunsetUpdateDTO sunsetUpdateDTO = new SunsetUpdateDTO(
+                expectedDate,
+                expectedSunset,
+                1
+        );
+
+
+        when(cityRepositoryMock.findById(sunsetUpdateDTO.cityId())).thenReturn(Optional.empty());
+        when(sunsetRepository.findById(1L)).thenReturn(
+                Optional.of(
+                        new SunsetEntity()
+                )
+        );
+
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
+
+        assertThrows(NonExistingCityException.class, () -> sunsetService.updateSunset(1, sunsetUpdateDTO));
+    }
+
+
+    @Test
+    void updateSunrise_SunriseRepositoryShouldReturnEmptyOptional_ShouldThrowNonExistingSunriseException() {
+        LocalDate expectedDate = LocalDate.now();
+        String expectedSunset = "6 AM";
+
+        SunsetUpdateDTO sunsetUpdateDTO = new SunsetUpdateDTO(
+                expectedDate,
+                expectedSunset,
+                1
+        );
+
+        when(sunsetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        SunsetService sunsetService = new SunsetService(sunsetRepository, restTemplateMock, cityRepositoryMock);
+
+        assertThrows(NonExistingSunsetException.class, () -> sunsetService.updateSunset(1, sunsetUpdateDTO));
+    }
 }
