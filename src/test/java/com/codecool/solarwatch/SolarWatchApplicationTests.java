49be7be63e2467cf01408ceb733b865e012061a4
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-20 11:20:28 +0100

    Fix the configuration and test error

--- src/test/java/com/codecool/solarwatch/SolarWatchApplicationTests.java
+++ src/test/java/com/codecool/solarwatch/SolarWatchApplicationTests.java
@@ -2,8 +2,10 @@ package com.codecool.solarwatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SolarWatchApplicationTests {
 
 	@Test
