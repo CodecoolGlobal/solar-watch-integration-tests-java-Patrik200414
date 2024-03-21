fb7402ab07bca33ec3214dab9def789cc7d1a314
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-21 21:50:24 +0100

    Refactor CityContoller

---
+++ src/test/java/com/codecool/solarwatch/controller/CityControllerIT.java
@@ -0,0 +1,18 @@
+package com.codecool.solarwatch.controller;
+
+import org.junit.jupiter.api.Test;
+
+class CityControllerIT {
+
+    @Test
+    void createCity() {
+    }
+
+    @Test
+    void updateCity() {
+    }
+
+    @Test
+    void deleteCity() {
+    }
+}
