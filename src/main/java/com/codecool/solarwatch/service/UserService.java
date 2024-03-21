b0f445e58825bc24754b7f7f663efa33e5f14159
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-21 20:13:55 +0100

    Implement UserService registrate method

--- src/main/java/com/codecool/solarwatch/service/UserService.java
+++ src/main/java/com/codecool/solarwatch/service/UserService.java
@@ -1,2 +1,34 @@
-package com.codecool.solarwatch.service;public class UserService {
+package com.codecool.solarwatch.service;
+
+import com.codecool.solarwatch.model.dto.user.Role;
+import com.codecool.solarwatch.model.dto.user.UserAuthenticationDTO;
+import com.codecool.solarwatch.model.entity.user.UserEntity;
+import com.codecool.solarwatch.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registrate(UserAuthenticationDTO userAuthenticationDTO){
+        UserEntity userEntity = new UserEntity(userAuthenticationDTO.userName(),
+                passwordEncoder.encode(userAuthenticationDTO.password()),
+                Set.of(Role.USER)
+        );
+
+        userRepository.save(userEntity);
+    }
+
+
 }
